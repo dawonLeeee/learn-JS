40b33ffbd49526814cae20e387ec0828818caddb
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-16 18:51:10 +0900

    make login-form

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const h1 = document.querySelector("div.hello:first-child h1"); /// # : id를 의미
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"; 
+const USERNAME_KEY = "username";
 
-
-// classList : HTML element의 class에 포함되어 있는지 보여줌
-function handleTitleClick() {
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+function onLoginSubmint(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
 
-h1.addEventListener("click", handleTitleClick);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmint);
+} else {
+    paintGreetings(savedUsername);
+}
 
 
