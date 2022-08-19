4af4a11311d19ba108840dfb5b8dd894181a8def
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-19 19:27:38 +0900

    JS로 화면 만들기(완성)

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-  const date = new Date(); // 현재 시스템의 시간을 알려줌
+  const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  //number->String 변경원할시 number format을 String()으로 감싸면 됨.
-
-  /*
-padStart, padEnd : 문자열의 길이를 원하는만큼 조정 가능
-1 --> 01로 조정 가능("1".padStart(2, "0"));
-*/
-
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
 getClock();
-setInterval(getClock, 1000); // function을 설정한 시간마다 반복하게함
+setInterval(getClock, 1000);
--- js/greeting.js
+++ js/greeting.js
@@ -1,10 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmint(event) {
+function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
@@ -21,7 +22,7 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmint);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
 }
